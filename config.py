1cf36d953f79febf35b6a3657d5d0a57f98269c0
priyankkapadia22 <144666968+priyankkapadia22@users.noreply.github.com>
2025-05-21 22:37:26 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
-apiKey = "Your API key"
+# todo: Add your api key here
+apikey = "AIzaSyAt3Y63IE9-MUz6xu6K2YU559hHmeowWb0"
--- main.py
+++ main.py
@@ -1,62 +1,118 @@
-import webbrowser  # pip install webbrowser
-import google.generativeai as genai  # pip install google-generativeai
+import speech_recognition as sr
 import os
-import win32com.client  # pip install win32com.client
-import speech_recognition as sr  # speech_recognition
-from config import apiKey
+import webbrowser
+import openai
+from config import apikey
+import datetime
+import random
+import numpy as np
 
-# Replace with your actual API key
-gemini_api_key = apiKey
 
-genai.configure(api_key=gemini_api_key)
-model = genai.GenerativeModel('gemini-pro')  # Or other model name
+chatStr = ""
+# https://youtu.be/Z3ZAJoi4x6Q
+def chat(query):
+    global chatStr
+    print(chatStr)
+    openai.api_key = apikey
+    chatStr += f"Harry: {query}\n Jarvis: "
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt= chatStr,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    # todo: Wrap this inside of a  try catch block
+    say(response["choices"][0]["text"])
+    chatStr += f"{response['choices'][0]['text']}\n"
+    return response["choices"][0]["text"]
 
-speaker = win32com.client.Dispatch("SAPI.SpVoice")
 
 def ai(prompt):
-    response = model.generate_content(query)
-    print(response.text)
-    speaker.Speak(response.text)
-    if not os.path.exists("Gemini"):
-        os.mkdir("Gemini")
+    openai.api_key = apikey
+    text = f"OpenAI response for Prompt: {prompt} \n *************************\n\n"
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    # todo: Wrap this inside of a  try catch block
+    # print(response["choices"][0]["text"])
+    text += response["choices"][0]["text"]
+    if not os.path.exists("Openai"):
+        os.mkdir("Openai")
 
     # with open(f"Openai/prompt- {random.randint(1, 2343434356)}", "w") as f:
-    with open(f"Gemini/{''.join(prompt.split('ai')[1:]).strip() }.txt", "w") as f:
-        f.write(response.text)
+    with open(f"Openai/{''.join(prompt.split('intelligence')[1:]).strip() }.txt", "w") as f:
+        f.write(text)
 
+def say(text):
+    os.system(f'say "{text}"')
 
 def takeCommand():
-    # Take command from user
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Listening...")
-        r.pause_threshold = 1
+        # r.pause_threshold =  0.6
         audio = r.listen(source)
+        try:
+            print("Recognizing...")
+            query = r.recognize_google(audio, language="en-in")
+            print(f"User said: {query}")
+            return query
+        except Exception as e:
+            return "Some Error Occurred. Sorry from Jarvis"
 
-    try:
-        print("Recognizing...")
-        query = r.recognize_google(audio, language='en-in')
-        print(f"User said: {query}\n")
-
-    except Exception:
-        speaker.Speak("Say that again please...")
-        return 'None'
-    return query
-
-
-if __name__ == "__main__":
-    speaker.Speak("Good Morning! I am Jarvis Ai! How may I help you?")
+if __name__ == '__main__':
+    print('Welcome to Jarvis A.I')
+    say("Jarvis A.I")
     while True:
-        query = takeCommand().lower()
+        print("Listening...")
+        query = takeCommand()
+        # todo: Add more sites
+        sites = [["youtube", "https://www.youtube.com"], ["wikipedia", "https://www.wikipedia.com"], ["google", "https://www.google.com"],]
+        for site in sites:
+            if f"Open {site[0]}".lower() in query.lower():
+                say(f"Opening {site[0]} sir...")
+                webbrowser.open(site[1])
+        # todo: Add a feature to play a specific song
+        if "open music" in query:
+            musicPath = "/Users/harry/Downloads/downfall-21371.mp3"
+            os.system(f"open {musicPath}")
 
-        if "Open Youtube".lower() in query.lower():
-            speaker.Speak("Opening Youtube...")
-            webbrowser.open("https://youtube.com")
+        elif "the time" in query:
+            musicPath = "/Users/harry/Downloads/downfall-21371.mp3"
+            hour = datetime.datetime.now().strftime("%H")
+            min = datetime.datetime.now().strftime("%M")
+            say(f"Sir time is {hour} bajke {min} minutes")
 
-        elif 'close' in query:
-            speaker.Speak("Thank you sir! Good bye!")
-            exit()
+        elif "open facetime".lower() in query.lower():
+            os.system(f"open /System/Applications/FaceTime.app")
 
-        elif 'ai'.lower() in query.lower():
+        elif "open pass".lower() in query.lower():
+            os.system(f"open /Applications/Passky.app")
+
+        elif "Using artificial intelligence".lower() in query.lower():
             ai(prompt=query)
 
+        elif "Jarvis Quit".lower() in query.lower():
+            exit()
+
+        elif "reset chat".lower() in query.lower():
+            chatStr = ""
+
+        else:
+            print("Chatting...")
+            chat(query)
+
+
+
+
+
+        # say(query)
